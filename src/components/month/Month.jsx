359a8c2058b20cffbb8990af3c202b604148d10a
Brage Rosberg <brage70@gmail.com>
2020-09-15 12:52:21 +0200

    Working on adding transactions for the monthly budget

--- src/components/month/Month.jsx
+++ src/components/month/Month.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './month.css';
 
 const Month = (props) => {
+  const [editMonth, editMonthState] = useState(false);
+  const [usedMonth, setMonthUsed] = useState(null);
+  const [remainingMonth, updateMonthlyRemaining] = useState(0);
+
+  useEffect(() => {
+    updateMonthlyRemaining(props.monthlyBudget);
+  }, [editMonth]);
+
   const clickHandler = () => {
-    console.log(`You clicked ${props.id}`);
-    console.log(`You clicked ${props.month}`);
+    editMonthState(!editMonth);
   }
-  return (
-    <section className="month--section" onClick={clickHandler}>
+
+  const subtractTester = () => {
+    updateMonthlyRemaining(remainingMonth - 1000);
+    setMonthUsed(usedMonth + 1000);
+  }
+
+
+  return editMonth ? (
+    <section className="month--section">
       <h2 className="month__name">{props.month}</h2>
       <p>{props.monthlyBudget}</p>
+      <p>Used {usedMonth}</p>
+      <p>Remaining {remainingMonth}</p>
+      <button onClick={subtractTester}>Use 1k</button>
+      <button onClick={clickHandler}>Toggle</button>
+    </section>
+  ) : (
+    <section className="month--section">
+      <h2 className="month__name">{props.month}</h2>
+      <p>{props.monthlyBudget}</p>
+      <button onClick={clickHandler}>Toggle</button>
     </section>
   )
 }
