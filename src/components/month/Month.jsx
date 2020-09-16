1f82a62bd59d8e5a600a0da2ee45d19b40184300
Brage Rosberg <brage70@gmail.com>
2020-09-16 11:37:58 +0200

    Made the UseEffect so it correctly accumulates the total sum of expenses in the Month components.

--- src/components/month/Month.jsx
+++ src/components/month/Month.jsx
@@ -3,37 +3,80 @@ import './month.css';
 
 const Month = (props) => {
   const [editMonth, editMonthState] = useState(false);
-  const [usedMonth, setMonthUsed] = useState(null);
+  const [usedMonth, setMonthUsed] = useState(0);
   const [remainingMonth, updateMonthlyRemaining] = useState(props.monthlyBudget);
+  const [expenses, setExpenses] = useState([]);
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
 
   useEffect(() => {
-    updateMonthlyRemaining(props.monthlyBudget-usedMonth);
-  }, [editMonth]);
+    updateMonthlyRemaining(props.monthlyBudget - usedMonth);
+  }, [props.monthlyBudget]);
+
+  useEffect(() => {
+    const total = expenses.reduce((accumulator, currentValue) => accumulator += parseInt(currentValue.amount), 0);
+    setMonthUsed(total);
+  }, [expenses]);
+
+  const handleClearExpenses = () => {
+    setExpenses([]);
+  }
 
   const clickHandler = () => {
     editMonthState(!editMonth);
   }
 
-  const subtractTester = () => {
-    updateMonthlyRemaining(remainingMonth - 1000);
-    setMonthUsed(usedMonth + 1000);
+  const handleName = event => {
+    setName(event.target.value)
+  }
+  
+  const handleAmount = event => {
+    setAmount(event.target.value)
   }
 
+  const handleSubmitForm = event => {
+    event.preventDefault()
+    if (name !== '' && amount > 0) {
+      const expense = { name, amount }
+      setExpenses([...expenses, expense])
+      setName('')
+      setAmount('')
+    } else {
+      console.log('Invalid expense name or the amount')
+    }
+  }
 
   return editMonth ? (
     <section className="month--section">
       <h2 className="month__name">{props.month}</h2>
-      <p>{props.monthlyBudget}</p>
-      <p>Used {usedMonth}</p>
-      <p>Remaining {remainingMonth}</p>
-      <button onClick={subtractTester}>Use 1k</button>
-      <button onClick={clickHandler}>Toggle</button>
+      <aside className="month__expenses">
+        <p>Budget: {props.monthlyBudget}</p>
+        <p>Remaining: {remainingMonth}</p>
+        <p>Used: {usedMonth}</p>
+        <div className="month__expenses--table">
+          {expenses.map(exp => (
+            <>
+              <p className="month__expenses--table__name">{exp.name}</p>
+              <p className="month__expenses--table__amount">{exp.amount}</p>
+            </>
+          ))}
+          <button onClick={handleClearExpenses}>Clear Expenses</button>
+        </div>
+        <div>
+          <form>
+            <input type="text" name="name" placeholder="Name of expense" value={name} onChange={handleName}/>
+          </form>
+          <form onSubmit={handleSubmitForm}>
+            <input type="number" name="amount" placeholder="0.00" value={amount} onChange={handleAmount}/>
+          </form>
+        </div>
+      </aside>
     </section>
   ) : (
     <section className="month--section">
       <h2 className="month__name">{props.month}</h2>
       <p>{props.monthlyBudget}</p>
-      <button onClick={clickHandler}>Toggle</button>
+      <button onClick={clickHandler}>Add expenses</button>
     </section>
   )
 }
