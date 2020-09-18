5b219a6ea422923adb585dfd33309f72e7d3c4a6
Brage Rosberg <brage70@gmail.com>
2020-09-18 12:35:40 +0200

    Refactored/Tidied the Form.jsx component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Form = (props) => {
   return (
-  <div> 
+  <aside> 
     <form>
-      <input type="text" name="name" placeholder="Name of expense" value={props.name} onChange={props.handleName}/>
+      <input type="text" name="name" placeholder="Enter the expense..." value={props.name} onChange={props.handleName}/>
     </form>
     <form onSubmit={props.handleSubmitForm}>
-      <input type="number" name="amount" placeholder="0.00" value={props.amount} onChange={props.handleAmount}/>
+      <input type="number" name="amount" placeholder="Enter the price..." value={props.amount} onChange={props.handleAmount}/>
     </form>
-  </div>
+  </aside>
   )
 }
 
