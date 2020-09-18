57f99aa07f8f1befdcb7d1793735f9b328d84518
Brage Rosberg <brage70@gmail.com>
2020-09-18 12:51:22 +0200

    Fixed deprecation warning og uuid, now using uuidv4() instead of uuid()

--- src/components/budget/year.js
+++ src/components/budget/year.js
@@ -1,55 +1,55 @@
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 const year = [
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'January'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'February'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'March'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'April'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'May'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'June'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'July'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'August'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'September'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'October'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'November'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'December'
   },
   {
-    key: uuid(),
+    key: uuidv4(),
     month: 'Buffer'
   },
 ]
