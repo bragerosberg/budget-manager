e48b92c65c514642ab84f210558e1385431c6a01
Brage Rosberg <brage70@gmail.com>
2020-09-22 11:45:25 +0200

    Updates

---
+++ precache-manifest.156222e4dd2f3435652584bf9b84bbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e336f85967f300e8c83b391a81d13ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63dea29abe97a75747f",
+    "url": "/static/css/main.d37af895.chunk.css"
+  },
+  {
+    "revision": "90f61398343c1b28269b",
+    "url": "/static/js/2.1f6866a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f6866a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63dea29abe97a75747f",
+    "url": "/static/js/main.c3ae38cd.chunk.js"
+  },
+  {
+    "revision": "0309d23ab0bb2e371d20",
+    "url": "/static/js/runtime-main.515be20a.js"
+  }
+]);
