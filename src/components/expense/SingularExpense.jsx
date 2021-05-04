88b339d1bc36ed3e206d4b59edeb5c245aa552d3
Brage Rosberg <brage70@gmail.com>
2021-05-04 23:04:47 +0200

    Refactor in multiple files, see commit for further details

--- src/components/expense/SingularExpense.jsx
+++ src/components/expense/SingularExpense.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const SingularExpense = (props) => (
+const SingularExpense = ({ name, amount, handleEditClick }) => (
   <div className="month__expenses--entry">
-     <p>{props.name}</p>
-      <p> - </p>
-      <p className="month__expenses--entry--amount">${props.amount}</p>
-      <button className="month__expenses--edit" onClick={props.handleEditClick}/>
+    <p>{name}</p>
+    <p> - </p>
+    <p className="month__expenses--entry--amount">${amount}</p>
+    <button className="month__expenses--edit" onClick={handleEditClick}/>
   </div>
 );
 
